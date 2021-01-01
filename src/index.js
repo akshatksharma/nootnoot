e102d006944dec025f30099621febf093ad992cb
Akshat Sharma <akshatksharma@wustl.edu>
2021-01-01 15:19:08 -0600

    Updated to use regex

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 
+const isUpperCase = (str) => str === str.toUpperCase();
+
 client.once("ready", () => {
   console.log("Ready!");
   client.user.setActivity("noot noot");
@@ -12,38 +14,47 @@ client.login("");
 let userCooldown = {};
 
 client.on("message", async (message) => {
-  if (message.content === "noot") {
-    if (message.member.voice.channel) {
-      try {
-        const connection = await message.member.voice.channel.join();
-        const rand = Math.random();
-        let file = "";
-
-        if (rand < 0.1) file = "src/noot.webm";
-        else if (rand < 0.2) file = "src/nootfry.webm";
-        else if (rand < 0.3) file = "src/noots_lol_1.webm";
-        else if (rand < 0.4) file = "src/noots_lol_2.webm";
-        else if (rand < 0.5) file = "src/noots_lol_3.webm";
-        else if (rand < 0.6) file = "src/noots_lol_4.webm";
-        else if (rand < 0.7) file = "src/noots_lol_5.webm";
-        else if (rand < 0.8) file = "src/noots_lol_6.webm";
-        else if (rand < 0.9) file = "src/noots_lol_7.webm";
-        else if (rand < 1) file = "src/noots_lol.webm";
-
-        connection.play(fs.createReadStream(file), {
-          type: "webm/opus",
-        });
-      } catch (error) {
-        console.log(error);
+  if (
+    message.content.length > 3 &&
+    message.content.match(/[n,N][o,O][o,O]+[T,t]/)
+  ) {
+    if (isUpperCase(message.content)) {
+      if (!userCooldown[message.author.id]) {
+        userCooldown[message.author.id] = true;
+        if (message.member.voice.channel) {
+          try {
+            const connection = await message.member.voice.channel.join();
+            connection.play(fs.createReadStream("src/nootfry.webm"), {
+              type: "webm/opus",
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
       }
-    }
-  } else if (message.content === "NOOT") {
-    if (!userCooldown[message.author.id]) {
-      userCooldown[message.author.id] = true;
+
+      setTimeout(() => {
+        userCooldown[message.author.id] = false;
+      }, 120000);
+    } else {
       if (message.member.voice.channel) {
         try {
           const connection = await message.member.voice.channel.join();
-          connection.play(fs.createReadStream("src/nootfry.webm"), {
+          const rand = Math.random();
+          let file = "";
+
+          if (rand < 0.1) file = "src/noot.webm";
+          else if (rand < 0.2) return;
+          else if (rand < 0.3) file = "src/noots_lol_1.webm";
+          else if (rand < 0.4) file = "src/noots_lol_2.webm";
+          else if (rand < 0.5) file = "src/noots_lol_3.webm";
+          else if (rand < 0.6) file = "src/noots_lol_4.webm";
+          else if (rand < 0.7) file = "src/noots_lol_5.webm";
+          else if (rand < 0.8) file = "src/noots_lol_6.webm";
+          else if (rand < 0.9) file = "src/noots_lol_7.webm";
+          else if (rand < 1) file = "src/noots_lol.webm";
+
+          connection.play(fs.createReadStream(file), {
             type: "webm/opus",
           });
         } catch (error) {
@@ -51,8 +62,5 @@ client.on("message", async (message) => {
         }
       }
     }
-    setTimeout(() => {
-      userCooldown[message.author.id] = false;
-    }, 120000);
   }
 });
